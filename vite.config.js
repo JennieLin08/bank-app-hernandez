9e772d5272e9ae94f45f06e417b0502759c4069e
jennieLin-08 <jennelyn.hernandez2578@gmail.com>
2023-10-04 20:50:57 +0800

    edit vite

--- vite.config.js
+++ vite.config.js
@@ -3,6 +3,6 @@ import react from '@vitejs/plugin-react'
 
 // https://vitejs.dev/config/
 export default defineConfig({
-  base: "./bank-app-hernandez",
+  base: "/JennieLin08/bank-app-hernandez",
   plugins: [react()],
 })
